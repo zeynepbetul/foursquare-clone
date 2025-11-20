19ddd3246f1c00ac1b6b14beeec9444348b52f80
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-20 17:47:44 +0300

    add login view elements

--- Foursquare/ViewController.swift
+++ Foursquare/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func signIn(_ sender: Any) {
+    }
+    
+    @IBAction func signUp(_ sender: Any) {
+    }
 }
 
