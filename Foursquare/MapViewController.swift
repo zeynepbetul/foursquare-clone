65d5f7758bbbba592dd52165680d9c085745788e
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-22 21:42:30 +0300

    add navigation right bar save button

--- Foursquare/MapViewController.swift
+++ Foursquare/MapViewController.swift
@@ -12,18 +12,12 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveClicked))
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func saveClicked() {
+        
     }
-    */
 
 }
