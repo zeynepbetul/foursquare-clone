3eb5aae5eb89b0e1f9e90b7dffa8f061f35e7a7c
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-22 22:52:22 +0300

    add details view controller view elements to the code

--- Foursquare/DetailsViewController.swift
+++ Foursquare/DetailsViewController.swift
@@ -6,24 +6,21 @@
 //
 
 import UIKit
+import MapKit
 
 class DetailsViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var placeName: UILabel!
+    @IBOutlet weak var atmosphere: UILabel!
+    @IBOutlet weak var placeType: UILabel!
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
