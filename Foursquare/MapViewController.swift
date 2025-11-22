2c284d06abd92208e426c7796e37960c5ae8810e
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-22 21:43:55 +0300

    import mapkit

--- Foursquare/MapViewController.swift
+++ Foursquare/MapViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
