ae2a23ff2af0941168f667bed8665ae60b115dfd
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-22 21:18:03 +0300

    add view elements to the  places vc

--- Foursquare/AddPlaceViewController.swift
+++ Foursquare/AddPlaceViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class AddPlaceViewController: UIViewController {
 
+    @IBOutlet weak var placeName: UITextField!
+    @IBOutlet weak var placeType: UITextField!
+    @IBOutlet weak var atmosphere: UITextField!
+    @IBOutlet weak var selectImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func nextButton(_ sender: Any) {
     }
-    */
-
+    
 }
