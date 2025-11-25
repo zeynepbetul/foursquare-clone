7bab1449e8565eb428816e73f6deb98842903e96
zeynepbetul <z.beetulkaya@gmail.com>
2025-11-25 13:41:44 +0300

    add class PlaceModel

---
+++ Foursquare/PlaceModel.swift
@@ -0,0 +1,22 @@
+//
+//  PlaceModel.swift
+//  Foursquare
+//
+//  Created by Zeynep Betül Kaya on 25.11.2025.
+//
+
+import Foundation
+import UIKit
+
+class PlaceModel {
+    
+    static let sharedInstance = PlaceModel()
+    var placeName: String?
+    var placeType: String?
+    var atmosphere: String?
+    var image: UIImage?
+    
+    private init() {
+        
+    }
+}
